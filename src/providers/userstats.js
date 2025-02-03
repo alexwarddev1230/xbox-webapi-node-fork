c65bc0aa654bc4d05d04b1d688b34e864ca1873d
Alex Ward <alexward@MacBookPro.lan>
2025-02-03 14:23:37 -0800

    fix: update to allow multiple ids

--- src/providers/userstats.js
+++ src/providers/userstats.js
@@ -8,13 +8,34 @@ module.exports = function (client) {
 
     provider._headers['x-xbl-contract-version'] = 2
 
-    provider.getUserTitleStats = function (titleId) {
-        Debug('getUserTitleStats(' + titleId + ')')
+    // provider.getUserTitleStats = function (titleId) {
+    //     Debug('getUserTitleStats(' + titleId + ')')
+    //
+    //     return this.post(
+    //         '/batch',
+    //         `{"arrangebyfield":"xuid","xuids":["${this._client._authentication._user.xid}"],"groups":[{"name":"Hero","titleId":"${titleId}"}],"stats":[{"name":"MinutesPlayed","titleId":"${titleId}"}]}`
+    //     )
+    // }
+
+    // allow multiple ids
+    provider.getUserTitleStats = function (titleIds) {
+        Debug('getUserTitleStats(' + titleIds + ')')
+
+        const titleIdArray = Array.isArray(titleIds) ? titleIds : [titleIds];
+
+        // Create group and stats objects for all titleIds
+        const groups = titleIdArray.map(id => ({ name: "Hero", titleId: id }));
+        const stats = titleIdArray.map(id => ({ name: "MinutesPlayed", titleId: id }));
 
         return this.post(
             '/batch',
-            `{"arrangebyfield":"xuid","xuids":["${this._client._authentication._user.xid}"],"groups":[{"name":"Hero","titleId":"${titleId}"}],"stats":[{"name":"MinutesPlayed","titleId":"${titleId}"}]}`
-        )
+            JSON.stringify({
+                arrangebyfield: "xuid",
+                xuids: [this._client._authentication._user.xid],
+                groups: groups,
+                stats: stats
+            })
+        );
     }
 
     return provider
