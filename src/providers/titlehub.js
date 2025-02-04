8198400ada095fa70523787a2627aef2152f5382
Alex Ward <alexward@MacBookPro.lan>
2025-02-03 16:37:16 -0800

    fix: allow max items

--- src/providers/titlehub.js
+++ src/providers/titlehub.js
@@ -6,30 +6,24 @@ module.exports = function(client){
     var provider = BaseProvider(client)
     provider._endpoint = 'https://titlehub.xboxlive.com'
 
-    provider.getTitleHistory = function(){
-        Debug('getTitleHistory()')
+    provider.getTitleHistory = function(params = ['achievement', 'image', 'scid'], maxItems) {
+        Debug(`getTitleHistory(params: ${params.join(', ')}${maxItems ? `, maxItems: ${maxItems}` : ''})`)
 
-        var params = [
-            'achievement',
-            'image',
-            'scid',
-        ]
+        let url = `/users/xuid(${this._client._authentication._user.xid})/titles/titlehistory/decoration/${params.join(',')}`
 
-        return this.get('/users/xuid('+this._client._authentication._user.xid+')/titles/titlehistory/decoration/'+params.join(','))
+        if (maxItems !== undefined) {
+            url += `?maxItems=${maxItems}`
+        }
+
+        return this.get(url)
     }
 
-    provider.getTitleId = function(titleId){
-        Debug('getTitleId('+titleId+')')
+    provider.getTitleId = function(titleId, params = ['achievement', 'image', 'detail', 'scid', 'alternateTitleId']) {
+        Debug(`getTitleId(${titleId}, params: ${params.join(', ')})`)
 
-        var params = [
-            'achievement',
-            'image',
-            'detail',
-            'scid',
-            'alternateTitleId'
-        ]
-
-        return this.get('/users/xuid('+this._client._authentication._user.xid+')/titles/titleid('+titleId+')/decoration/'+params.join(','))
+        return this.get(
+            `/users/xuid(${this._client._authentication._user.xid})/titles/titleid(${titleId})/decoration/${params.join(',')}`
+        )
     }
 
     return provider
